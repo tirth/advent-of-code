a79bb9cca16aaf66c9bc3f61aee8fe72e0b95709
tirth <code@tirth.xyz>
2021-11-21 19:35:05 -0500

    Add day 3

--- day2.nim
+++ day2.nim
@@ -6,26 +6,26 @@ proc getDimensions(dimensions: string): tuple[l: int, w: int, h: int] =
         return (l, w, h)
 
 proc calculatePaperSize(dimensions: string): int =
-    let dims = getDimensions(dimensions)
+    let (l, w, h) = getDimensions(dimensions)
     
-    let lw = dims.l * dims.w
-    let wh = dims.w * dims.h
-    let hl = dims.h * dims.l
+    let lw = l * w
+    let wh = w * h
+    let hl = h * l
 
-    let smallestArea = min(@[lw, wh, hl])
+    let smallestArea = min([lw, wh, hl])
     
     return (2 * lw) + (2 * wh) + (2 * hl) + smallestArea
 
 proc calculateRibbonSize(dimensions: string): int =
-    let dims = getDimensions(dimensions)
+    let (l, w, h) = getDimensions(dimensions)
 
-    let lw = 2 * dims.l + 2 * dims.w
-    let wh = 2 * dims.w + 2 * dims.h
-    let hl = 2 * dims.h + 2 * dims.l
+    let lw = 2 * l + 2 * w
+    let wh = 2 * w + 2 * h
+    let hl = 2 * h + 2 * l
 
-    let smallestPerimeter = min(@[lw, wh, hl])
+    let smallestPerimeter = min([lw, wh, hl])
     
-    return dims.l * dims.w * dims.h + smallestPerimeter
+    return l * w * h + smallestPerimeter
 
 var totalPaper, totalRibbon = 0
 for giftSize in "day2_input.txt".lines:
---
+++ day3.nim
@@ -0,0 +1,51 @@
+import critbits
+
+proc updateLocation(direction: char, location: var tuple[x: int, y: int]) = 
+    case direction
+    of '^': inc(location.y)
+    of 'v': dec(location.y)
+    of '>': inc(location.x)
+    of '<': dec(location.x)
+    else: discard
+
+proc housesVisited(directions: string): int =
+    var location : tuple[x: int, y: int] = (0, 0)
+
+    var visited = [$location].toCritBitTree()
+
+    for direction in directions:
+        updateLocation(direction, location)
+
+        visited.incl($location)
+
+    return visited.len()
+
+type
+    Santa = enum
+        norm, robo
+
+proc roboHousesVisited(directions: string): int =
+    var normSanta : tuple[x: int, y: int] = (0, 0)
+    var roboSanta : tuple[x: int, y: int] = (0, 0)
+
+    var visited = [$normSanta].toCritBitTree()
+
+    var currentSanta = Santa.norm
+
+    for direction in directions:
+        case currentSanta
+        of norm:
+            updateLocation(direction, normSanta)
+            visited.incl($normSanta)
+            currentSanta = Santa.robo
+        of robo:
+            updateLocation(direction, roboSanta)
+            visited.incl($roboSanta)
+            currentSanta = Santa.norm
+
+    return visited.len()
+
+let directions = readFile("day3_input.txt")
+
+echo housesVisited(directions)
+echo roboHousesVisited(directions)

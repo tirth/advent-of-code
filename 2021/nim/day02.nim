52d3f2094ef03710aa289faf4a3464363237954e
tirth <code@tirth.xyz>
2021-12-03 03:36:32 -0500

    Add 2021 day 3, clean up day 2

--- 2021/nim/day02.nim
+++ 2021/nim/day02.nim
@@ -1,15 +1,11 @@
 import strutils
 
-proc move1(direction: string, amount: int, horizontalPos, depth: var int) =
-    case direction
-    of "forward":
-        horizontalPos += amount
-    of "down":
-        depth += amount
-    of "up":
-        depth -= amount
+var horizontalPos, depth, aim = 0
+for line in "../input/day2.txt".lines:
+    let sp = line.split(' ')
+    let direction = sp[0]
+    let amount = sp[1].parseInt
 
-proc move2(direction: string, amount: int, horizontalPos, depth, aim: var int) =
     case direction
     of "forward":
         horizontalPos += amount
@@ -19,14 +15,7 @@ proc move2(direction: string, amount: int, horizontalPos, depth, aim: var int) =
     of "up":
         aim -= amount
 
-var hor1, dep1, hor2, dep2, aim = 0
-for line in "../input/day2.txt".lines:
-    let sp = line.split(' ')
-    let dir = sp[0]
-    let amt = sp[1].parseInt
-
-    move1(dir, amt, hor1, dep1)
-    move2(dir, amt, hor2, dep2, aim)
-
-echo hor1 * dep1
-echo hor2 * dep2
+# horizontal position is the same for both parts,
+# aim acts as depth
+echo horizontalPos * aim
+echo horizontalPos * depth
---
+++ 2021/nim/day03.nim
@@ -0,0 +1,55 @@
+import strutils, sequtils, tables
+
+type
+    BitList = seq[int]
+    BitCount = CountTable[int]
+
+proc toInt(bitList: BitList): int =
+    bitList.join.parseBinInt
+
+proc getCountsAt(nums: seq[BitList], index: int): BitCount =
+    for num in nums:
+        result.inc(num[index])
+
+proc getCounts(nums: seq[BitList]): seq[BitCount] =
+    let bitWidth = nums[0].len
+    result.newSeq(bitWidth)
+
+    for i in 0..<bitWidth:
+        result[i] = nums.getCountsAt(i)
+
+proc searchByCriteria(nums: seq[BitList], byMostCommon: bool = true): BitList =
+    var searchNums = nums
+    for i in 0..nums[0].high:
+        let counts = searchNums.getCountsAt(i)
+        
+        let keep = if counts[0] > counts[1]: (if byMostCommon: 0 else: 1) else: (if byMostCommon: 1 else: 0)
+
+        var kept: seq[BitList]
+        for num in searchNums:
+            if num[i] == keep:
+                kept.add(num)
+
+        searchNums = kept
+
+        if searchNums.len == 1:
+            break
+
+    return searchNums[0]
+
+let nums = "../input/day3.txt".readFile.splitLines.mapIt(it.mapIt(if it == '1': 1 else: 0))
+
+let counts = nums.getCounts
+
+var most, least: BitList
+for c in counts:
+    if c[0] > c[1]:
+        most.add(0)
+        least.add(1)
+    else:
+        most.add(1)
+        least.add(0)
+
+echo most.toInt * least.toInt
+
+echo nums.searchByCriteria.toInt * nums.searchByCriteria(false).toInt

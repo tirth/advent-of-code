726aa2796a05ce7f239baaced986b6069776e4be
tirth <code@tirth.xyz>
2021-12-14 03:19:50 -0500

    Clean up 2021 day 14

--- 2021/nim/day14.nim
+++ 2021/nim/day14.nim
@@ -1,32 +1,26 @@
 import strutils, tables
 
-proc doStep(poly: string, rules: Table[string, string]): string =
+proc doStep(poly: string, rules: Table[string, char]): string =
     result.add poly[0]
 
     for i in 0 ..< poly.high:
         result.add rules[poly[i..i + 1]] & poly[i + 1]
 
-proc doStepCounts(pairCounts: Table[string, int], rules: Table[string, char]): Table[string, int] =
-    result = pairCounts
-
+proc doStepCounts(pairCounts: CountTable[string], rules: Table[string, char]): CountTable[string] =
     for pair, count in pairCounts:
-        if count < 1:
-            continue
-
         let l = rules[pair]
-        dec(result[pair], count)
-        inc(result[pair[0] & l], count)
-        inc(result[l & pair[1]], count)
+        result.inc(pair[0] & l, count)
+        result.inc(l & pair[1], count)
 
-proc getCharCounts(pairCounts: Table[string, int], firstChar: char): CountTable[char] =
-    result = [firstChar].toCountTable
+proc getCharCounts(pairCounts: CountTable[string], firstChar: char): CountTable[char] =
+    result.inc(firstChar)
 
     for pair, count in pairCounts:
         result.inc(pair[1], count)
 
 var poly = ""
 var rules: Table[string, char]
-var pairCounts: Table[string, int]
+var pairCounts: CountTable[string]
 
 # read input
 for line in "../input/day14.txt".lines:
@@ -37,11 +31,10 @@ for line in "../input/day14.txt".lines:
     if not line.isEmptyOrWhitespace:
         let rSplit = line.split(" -> ")
         rules[rSplit[0]] = rSplit[1][0]
-        pairCounts[rSplit[0]] = 0
 
 # initialize pair counts
 for i in 0 ..< poly.high:
-    inc pairCounts[poly[i..i + 1]]
+    pairCounts.inc(poly[i..i + 1])
 
 # iterate
 for i in 1..40:
